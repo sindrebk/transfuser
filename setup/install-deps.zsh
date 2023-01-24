2e026371f684619da84dd934b93bfc0370b93917
Sindre Kummervold <sindrebk@gmail.com>
2023-01-24 13:53:00 +0100

    fix: allow scripts to be executed

--- setup/install-deps.zsh
+++ setup/install-deps.zsh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 cd "$(dirname "$0")"
 
 if [ -z "$VIRTUAL_ENV" ]; then
@@ -7,45 +9,15 @@ if [ -z "$VIRTUAL_ENV" ]; then
     exit 1
 fi
 
-export CUDA_VERSION_SHORT=$(
-    CUDA_PATH=$(realpath /usr/local/cuda)
-    CUDA_NAME=$(basename $CUDA_PATH)
-    echo $CUDA_NAME | sed -e 's/^cuda-/cu/' | tr -d '.'
-)
-
-export TORCH_VERSION=1.12.0
-
-case $CUDA_VERSION_SHORT in
-    cu113)
-        export REQUIREMENTS_TORCH_OPTS="--extra-index-url https://download.pytorch.org/whl/${CUDA_VERSION_SHORT}"
-        export REQUIREMENTS_MMCV_OPTS="-f https://download.openmmlab.com/mmcv/dist/${CUDA_VERSION_SHORT}/torch${TORCH_VERSION}/index.html"
-        export REQUIREMENTS_TORCH_SCATTER_OPTS="-f https://data.pyg.org/whl/torch-${TORCH_VERSION}+${CUDA_VERSION_SHORT}.html"
-        ;;
-    cu116)
-        # todo: see if MMCV cu115 binaries are compatible
-        export REQUIREMENTS_TORCH_OPTS="--extra-index-url https://download.pytorch.org/whl/${CUDA_VERSION_SHORT}"
-        export REQUIREMENTS_MMCV_OPTS=""
-        export REQUIREMENTS_TORCH_SCATTER_OPTS="-f https://data.pyg.org/whl/torch-${TORCH_VERSION}+${CUDA_VERSION_SHORT}.html"
-        ;;
-    *)
-        echo "Unsupported CUDA version: \"$CUDA_VERSION_SHORT\""
-        exit 1
-        ;;
-esac
-
-echo "Found CUDA version: \"$CUDA_VERSION_SHORT\""
-
 export MAKEFLAGS="-j$(nproc)"
 
 echo
 echo "Upgrading pip"
-pip install --upgrade pip
+pip install --upgrade pip wheel setuptools
 
-for stage in 1 2 3 4; do
-    echo
-    echo "Installing stage $stage dependencies..."
-    pip install --upgrade -r requirements-stage-$stage.txt
-done
+echo
+echo "Installing requirements"
+pip install -r requirements.txt
 
 echo
 echo "Fixing opencv-python-headless"
